a4b209a42554cbce6ec2576e469e835df189c234
MiotyRan <miotisoaranarimanana@gmail.com>
2025-08-22 17:26:33 +0300

    mainJSX

--- src/main.jsx
+++ src/main.jsx
@@ -11,9 +11,9 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
-import {AppProvider} from './context/AppContext.jsx'
+import './index.css'
+import { AppProvider } from './context/AppContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
