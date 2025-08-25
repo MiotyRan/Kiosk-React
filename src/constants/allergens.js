09770f0dfd71d2251c332813b21b31de0e276c56
MiotyRan <miotisoaranarimanana@gmail.com>
2025-08-25 14:26:05 +0300

    allergensJS

---
+++ src/constants/allergens.js
@@ -0,0 +1,17 @@
+// Configuration des allergènes (14 obligatoires UE)
+export const ALLERGENS = {
+    1: { code: 'GLU', name: 'Gluten', icon: '🌾' },
+    2: { code: 'CRU', name: 'Crustacés', icon: '🦐' },
+    3: { code: 'EGG', name: 'Œufs', icon: '🥚' },
+    4: { code: 'FSH', name: 'Poisson', icon: '🐟' },
+    5: { code: 'PNT', name: 'Arachides', icon: '🥜' },
+    6: { code: 'SOY', name: 'Soja', icon: '🌱' },
+    7: { code: 'MLK', name: 'Lait', icon: '🥛' },
+    8: { code: 'NUT', name: 'Fruits à coque', icon: '🌰' },
+    9: { code: 'CEL', name: 'Céleri', icon: '🥬' },
+    10: { code: 'MUS', name: 'Moutarde', icon: '🟡' },
+    11: { code: 'SES', name: 'Graines de sésame', icon: '⚪' },
+    12: { code: 'SUL', name: 'Sulfites', icon: '🧪' },
+    13: { code: 'LUP', name: 'Lupin', icon: '🌿' },
+    14: { code: 'MOL', name: 'Mollusques', icon: '🦪' }
+};
